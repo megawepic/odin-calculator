0dcb4e2aeb06d7968146395c9640b714226edb4f
megawepic <efps10.5pyapdarren@gmail.com>
2025-12-20 22:34:48 +0800

    edit code.js index.html style.css

--- code.js
+++ code.js
@@ -13,3 +13,15 @@ function divide(a,b){
 function multiply(a,b) {
     return a * b
 }
+
+function operate(num1,num2,operator){
+    if (operator == "+"){
+        return add(num1, num2)
+    } else if (operator == "-"){
+        return subtract(num1,num2)
+    } else if (operator == "/") {
+        return divide(num1,num2)
+    } else if (operator == "*"){
+        return multiply(num1,num2)
+    }
+}
